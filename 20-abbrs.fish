2d65ba0a94dd01d45d7304200fc927d276fae968
Félix Saparelli <felix@passcod.name>
2022-03-17 14:39:48 +1300

    Too many git commit abbrs

--- 20-abbrs.fish
+++ 20-abbrs.fish
@@ -5,8 +5,11 @@ abbr + 'git add'
 abbr ↓ 'git up'
 abbr ↑ 'git push'
 abbr ↑↑↑ 'git push --force-with-lease'
-abbr ← 'git commit -am'
-abbr ←←← 'git commit --amend -aC HEAD'
+abbr ← 'git commit -m'
+abbr ←← 'git commit -am'
+abbr ←←← 'git commit --amend'
+abbr ←←←← 'git commit --amend -C HEAD'
+abbr ←←←←← 'git commit --amend -C HEAD -a'
 abbr atom. 'atom .'
 abbr code. 'code .'
 abbr b bundle
@@ -22,7 +25,7 @@ abbr g git
 abbr j journalctl
 abbr jf 'journalctl -f'
 abbr jfu 'journalctl -fu'
-abbr k kubectl
+abbr k kalker
 abbr la 'lx -SalHigh --git'
 abbr ls lx
 abbr n pnpm
